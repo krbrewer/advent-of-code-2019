4cc89a73ea728af72b89a27819c88e234547a5fb
Kevin Brewer <kevinbrewer@kevins-mbp.lan>
2019-12-14 11:01:26 -0600

    day 1 solution

--- day1/index.js
+++ day1/index.js
@@ -1,8 +1,19 @@
-import { readFile } from 'fs';
-readFile('input.txt', (err, data) => {
-    if (err) throw err;
-    var array = data.toString().split("\n");
-    for (i in array) {
-        console.log(array[i]);
-    }
-})
+import {readFileSync} from 'fs'
+
+const calculate = input => {
+    const data = readFileSync(input).toString().split("\n").map(num => parseInt(num))
+    const measurement = data.reduce((acc, curr) => {
+        let weight = Math.max(Math.floor(curr/3) - 2, 0)
+        let fuel = weight
+        let newFuel = 0
+        while (weight > 0) {
+            newFuel = Math.max(Math.floor(weight/3) - 2, 0)
+            fuel += newFuel
+            weight = newFuel
+        }
+        return acc + fuel
+    }, 0)
+    return measurement
+}
+
+console.log(calculate(process.argv[2]))
